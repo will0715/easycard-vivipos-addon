35e807c0a70245129d5c2a99b1c636de96cff60b
Jim Chen <jim.chen@vivicloud.net>
2017-05-18 14:02:26 +0800

    Add xml2json library, modify icerapi tag

--- chrome/content/easycard/ICERAPIRequest.js
+++ chrome/content/easycard/ICERAPIRequest.js
@@ -1,17 +1,15 @@
-var EDCRequest = function(tmId, tmAgentNum) {
-    this._tmId = tmId;
-    this._tmAgentNum = tmAgentNum;
+var ICERAPIRequest = function() {
 };
 
-EDCRequest.prototype = {
+ICERAPIRequest.prototype = {
 
     PROCESS_CODE: {
         "signon": "881999",
-        "payout": "811599",
-        "cancel": "823899",
-        "query": "999999",
-        "balance": "200000",
-        "settlement": "900000"
+        "payout": "606100",
+        "cancel": "620061",
+        "query": "216000",
+        "balance": "296000",
+        "settlement": "900099"
     },
 
     MESSAGE_TYPE: {
@@ -23,12 +21,14 @@ EDCRequest.prototype = {
      * get the payout's request xml string
      * @param {Integer} amount
      * @param {String} serialNum
+     * @param {String} HOST serialNum
      * @param {String} transactionSeq
      * @return {String} xml
      */
-    payoutRequest: function(amount, serialNum, transactionSeq) {
+    payoutRequest: function(amount, serialNum, hostSerialNum, transactionSeq) {
         let requestBody = "<T0400>" + amount + "</T0400>";
-        requestBody += "<T1101>" + serialNum + "</T1101>";
+        requestBody += "<T1100>" + serialNum + "</T1100>";
+        requestBody += "<T1101>" + hostSerialNum + "</T1101>";
         requestBody += "<T3701>" + transactionSeq + "</T3701>";
         return this._buildRequestXml(this.MESSAGE_TYPE.request, this.PROCESS_CODE.payout, requestBody);
     },
@@ -36,12 +36,14 @@ EDCRequest.prototype = {
      * get the cancel's request xml string
      * @param {Integer} amount
      * @param {String} serialNum
+     * @param {String} HOST serialNum
      * @param {String} transactionSeq
      * @return {String} xml
      */
-    cancelRequest: function(amount, serialNum, transactionSeq) {
+    cancelRequest: function(amount, serialNum, hostSerialNum, transactionSeq) {
         let requestBody = "<T0400>" + amount + "</T0400>";
-        requestBody += "<T1101>" + serialNum + "</T1101>";
+        requestBody += "<T1100>" + serialNum + "</T1100>";
+        requestBody += "<T1101>" + hostSerialNum + "</T1101>";
         requestBody += "<T3701>" + transactionSeq + "</T3701>";
         return this._buildRequestXml(this.MESSAGE_TYPE.request, this.PROCESS_CODE.cancel, requestBody);
     },
@@ -49,41 +51,46 @@ EDCRequest.prototype = {
      * get the query's request xml string
      * @param {Integer} amount
      * @param {String} serialNum
-     * @param {String} transactionSeq
+     * @param {String} HOST serialNum
      * @return {String} xml
      */
-    queryRequest: function(amount, serialNum, transactionSeq) {
+    queryRequest: function(amount, serialNum, hostSerialNum) {
         let requestBody = "<T0400>" + amount + "</T0400>";
-        requestBody += "<T1101>" + serialNum + "</T1101>";
-        requestBody += "<T3701>" + transactionSeq + "</T3701>";
+        requestBody += "<T1100>" + serialNum + "</T1100>";
+        requestBody += "<T1101>" + hostSerialNum + "</T1101>";
         return this._buildRequestXml(this.MESSAGE_TYPE.request, this.PROCESS_CODE.query, requestBody);
     },
     /**
      * get the balance's request xml string
+     * @param {String} serialNum
+     * @param {String} HOST serialNum
      * @return {String} xml
      */
-    balanceRequest: function() {
-        return this._buildRequestXml(this.MESSAGE_TYPE.request, this.PROCESS_CODE.balance);
+    balanceRequest: function(serialNum, hostSerialNum) {
+        let requestBody = "<T1100>" + serialNum + "</T1100>";
+        requestBody += "<T1101>" + hostSerialNum + "</T1101>";
+        return this._buildRequestXml(this.MESSAGE_TYPE.request, this.PROCESS_CODE.balance, requestBody);
     },
     /**
      * get the settlement's request xml string
      * @param {String} serialNum
+     * @param {String} HOST serialNum
      * @return {String} xml
      */
-    settlementRequest: function(serialNum) {
-        let requestBody = "<T1101>" + serialNum + "</T1101>";
+    settlementRequest: function(serialNum, hostSerialNum) {
+        let requestBody = "<T1100>" + serialNum + "</T1100>";
+        requestBody += "<T1101>" + hostSerialNum + "</T1101>";
         return this._buildRequestXml(this.MESSAGE_TYPE.settlement, this.PROCESS_CODE.settlement, requestBody);
     },
     /**
      * get the signon's request xml string
      * @param {String} serialNum
-     * @param {String} signOnPwd
+     * @param {String} HOST serialNum
      * @return {String} xml
      */
-    signonRequest: function(serialNum, signOnPwd) {
-        let requestBody = "<T1101>" + serialNum + "</T1101>";
-        requestBody += "<T5595><T559502>PWD</T559502>";
-        requestBody += "<T559503>" + signOnPwd + "</T559503></T5595>";
+    signonRequest: function(serialNum, hostSerialNum) {
+        let requestBody = "<T1100>" + serialNum + "</T1100>";
+        requestBody += "<T1101>" + hostSerialNum + "</T1101>";
         return this._buildRequestXml(this.MESSAGE_TYPE.signon, this.PROCESS_CODE.signon, requestBody);
     },
     /**
@@ -94,17 +101,15 @@ EDCRequest.prototype = {
      * @return {String} xml
      */
     _buildRequestXml: function(messageType, processCode, requestBody) {
-        let request = "<TM><TRANS>";
+        let request = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?><TransXML><TRANS>';
         request += "<T0100>" + messageType + "</T0100>";
         request += "<T0300>" + processCode + "</T0300>";
         if (typeof requestBody != "undefined") {
-            request += "<T1201>" + (new Date()).toString('HHmmss') + "</T1201>";
-            request += "<T1301>" + (new Date()).toString('yyyyMMdd') + "</T1301>";
-            request += "<T5504>" + this._tmId + "</T5504>";
-            request += "<T5510>" + this._tmAgentNum + "</T5510>";
+            request += "<T1200>" + (new Date()).toString('HHmmss') + "</T1200>";
+            request += "<T1300>" + (new Date()).toString('yyyyMMdd') + "</T1300>";
             request += requestBody;
         }
-        request += "</TRANS></TM>";
+        request += "</TRANS></TransXML>";
         return request;
     }
 
--- chrome/content/easycard/ICERAPIResponse.js
+++ chrome/content/easycard/ICERAPIResponse.js
@@ -1,11 +1,12 @@
-var EDCResponse = {
+var ICERAPIResponse = {
 
     KEY_CARD_PHYSICAL_ID: "T0200",
     KEY_TXN_AMOUNT: "T0400",
+    KEY_AUTOLOAD_TXN_AMOUNT: "T0409",
     KEY_BALANCE: "T0410",
     KEY_RESPONSE_CODE: "T3900",
+    KEY_RETURN_CODE: "T3901",
     KEY_REFERENCE_NUM: "T3700",
-    KEY_ERROR_MSG: "ErrMsg",
 
     CODE_SUCCESS: "0",
 
@@ -15,7 +16,7 @@ var EDCResponse = {
      * @return {Object | null}
      */
     parseResponse: function(responseJSON) {
-        return (typeof responseJSON != "undefined" && responseJSON.response != null && typeof responseJSON.response.TRANS != "undefined") ? responseJSON.response.TRANS : null;
+        return (typeof responseJSON != "undefined" && typeof responseJSON.TransXML != 'undefined' && typeof responseJSON.TransXML.TRANS != 'undefined') ? responseJSON.TransXML.TRANS : null;
     }
 
 };
